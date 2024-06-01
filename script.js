a917bb2bd816b344691e0016c3de67354633d634
EphRex <128737077+EphRex@users.noreply.github.com>
2024-06-01 21:58:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function message() {
   let IP = document.getElementById("IP").value;
 
   const webhook = "https://discord.com/api/webhooks/1246559249325097052/UzuMt97coTW1187qKif_WXZnHvLg4n-1C_M0NsBYtP2OBDDVv5RbZy3IZZMh33lzagkB";
-  const contents = 'IP: ${IP}';
+  const contents = 'IP:';
   const request = new XMLHttpRequest();
   request.open("POST", webhook);
   request.setRequestHeader('Content-type', 'application/json');
