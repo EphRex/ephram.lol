63f7142d40dfae069e819dfb85281ecb99b1956b
EphRex <128737077+EphRex@users.noreply.github.com>
2024-06-02 18:33:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const whurl = "https://discord.com/api/webhooks/1246559249325097052/UzuMt97coTW1187qKif_WXZnHvLg4n-1C_M0NsBYtP2OBDDVv5RbZy3IZZMh33lzagkB"
 
 const msg = {
-    "content":
-        "ip: ${ip}"
+    "content": "Hello! I'm a bot, this is fetch api"
 }
 
-fetch(whurl + "?wait=true", {
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json"
-        },
-        "body": JSON.stringify(msg)
-    })
-    .then(a = xa.json()).then(console.log)
+fetch(whurl + "?wait=true", 
+{"method":"POST", 
+"headers": {"content-type": "application/json"},
+"body": JSON.stringify(msg)})
+.then(a=xa.json()).then(console.log)
